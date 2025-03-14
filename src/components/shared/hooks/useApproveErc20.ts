4218686c7d17b204db2e41914593a352273afaad
Tarp <travistarpley0@gmail.com>
2025-03-14 10:41:43 -0700

    feat: display warning message for usdt edge case

--- src/components/shared/hooks/useApproveErc20.ts
+++ src/components/shared/hooks/useApproveErc20.ts
@@ -1,3 +1,4 @@
+import { env } from "@/env";
 import type { TAddressString } from "@/lib/types";
 import { useMemo } from "react";
 import { useSimulateContract } from "wagmi";
@@ -7,19 +8,27 @@ interface Props {
   amount: bigint;
   allowance: bigint;
 }
-const USDT_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const USDT_ADDRESS =
+  env.NEXT_PUBLIC_CHAIN_ID === "1"
+    ? "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+    : "0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0";
 export function useApproveErc20({
   amount,
   allowance,
   tokenAddr,
   approveContract,
 }: Props) {
+  console.log(tokenAddr === USDT_ADDRESS, tokenAddr, USDT_ADDRESS);
   // needs to check for 0 approval for usdt edge case
   const needs0Approval = useMemo(() => {
     if (allowance === undefined) {
       return false;
     }
-    if (allowance > 0n && tokenAddr === USDT_ADDRESS && allowance < amount) {
+    if (
+      allowance > 0n &&
+      tokenAddr.toLowerCase() === USDT_ADDRESS.toLowerCase() &&
+      allowance < amount
+    ) {
       return true;
     }
   }, [allowance, amount, tokenAddr]);
@@ -38,12 +47,8 @@ export function useApproveErc20({
   });
   return { approveSimulate, needsApproval, needs0Approval };
 }
-
 function getAbi(tokenAddr: TAddressString) {
-  if (
-    tokenAddr.toLowerCase() ===
-    "0xdAC17F958D2ee523a2206206994597C13D831ec7".toLowerCase()
-  ) {
+  if (tokenAddr.toLowerCase() === USDT_ADDRESS.toLowerCase()) {
     return nonStandardAbi;
   } else {
     return nonStandardAbi;
