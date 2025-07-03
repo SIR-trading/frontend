9b08141e94ff1fc38b83b0194243a13a8182cc61
James Osunkiyesi <jaydhales@gmail.com>
2025-07-03 23:12:40 +0100

    fix: adjust margin for header in AuctionContentWrapper

--- src/components/auction/auctionContentWrapper.tsx
+++ src/components/auction/auctionContentWrapper.tsx
@@ -10,10 +10,11 @@ const AuctionContentWrapper = ({
   return (
     <div>
       {header && (
-        <h3 className="mb-8 h-[32px] text-center font-geist text-[24px] font-normal leading-[32px] text-foreground">
+        <h3 className="mb-4 h-[32px] text-center font-geist text-[24px] font-normal leading-[32px] text-foreground">
           {header}
         </h3>
       )}
+      <div className="pb-4"></div>
       <div className="grid min-h-[172px] gap-4 gap-y-8 md:grid-cols-2 lg:gap-8">
         {children}
       </div>
