5285290343bc324da1e594ed7497e1bbc1452124
James Osunkiyesi <jaydhales@gmail.com>
2025-07-20 21:59:07 +0100

    feat: implement getActiveApePositions query and related calculations

--- src/server/api/routers/leaderboard.ts
+++ src/server/api/routers/leaderboard.ts
@@ -1,8 +1,14 @@
-import type { TClosedApePositions } from "@/lib/types";
+import { ApeContract } from "@/contracts/ape";
+import { AssistantContract } from "@/contracts/assistant";
+import type { TCurrentApePositions, TClosedApePositions } from "@/lib/types";
+import { multicall, readContract } from "@/lib/viemClient";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { getClosedApePositions } from "@/server/queries/leaderboard";
+import {
+  getClosedApePositions,
+  getCurrentApePositions,
+} from "@/server/queries/leaderboard";
 import groupBy from "lodash.groupby";
-import { formatUnits, getAddress } from "viem";
+import { formatEther, formatUnits, fromHex, getAddress } from "viem";
 
 const calculatePnl = (withdrawn: number, deposited: number) =>
   withdrawn - deposited;
@@ -11,6 +17,75 @@ const calculatePercentage = (withdrawn: number, deposited: number) =>
   deposited > 0 ? (withdrawn / deposited - 1) * 100 : 0;
 
 export const leaderboardRouter = createTRPCRouter({
+  getActiveApePositions: publicProcedure.query(async () => {
+    const { apePositions } = await getCurrentApePositions();
+
+    const apeTotalSupply = await multicall({
+      contracts: apePositions.map((position) => ({
+        address: position.apeAddress,
+        abi: ApeContract.abi,
+        functionName: "totalSupply",
+      })),
+    });
+
+    const vaultReserves = await readContract({
+      ...AssistantContract,
+      functionName: "getReserves",
+      args: [
+        apePositions.map((position) => fromHex(position.vaultId, "number")),
+      ],
+    });
+
+    const netPosition = apePositions.map(
+      (
+        {
+          apeBalance,
+          leverageTier,
+          vaultId,
+          user,
+          collateralTotal,
+          dollarTotal,
+        },
+        index,
+      ) => {
+        const totalSupply = BigInt(apeTotalSupply[index]?.result ?? 0n);
+        const collateralApeVault = vaultReserves[index]?.reserveApes ?? 0n;
+        const collateralPosition =
+          (BigInt(apeBalance) * collateralApeVault) / totalSupply;
+
+        const leverageMultiplier = BigInt(1 + (leverageTier - 1) * 10000);
+        const netCollateralPosition = collateralPosition / leverageMultiplier;
+
+        return {
+          vaultId,
+          user,
+          collateralPosition,
+          collateralTotal,
+          netCollateralPosition,
+          dollarTotal,
+        };
+      },
+    );
+
+    const groupedPositions = groupBy(netPosition, (position) =>
+      getAddress(position.user),
+    );
+
+    return Object.entries(groupedPositions).reduce<TCurrentApePositions>(
+      (acc, [user, userPositions]) => {
+        const totalNet = userPositions.reduce(
+          (res, { netCollateralPosition }) => res + netCollateralPosition,
+          0n,
+        );
+        acc[user] = {
+          pnlUsd: +formatEther(totalNet), // USD amounts always use 6 decimals
+          pnlUsdPercentage: +formatEther(totalNet),
+        };
+        return acc;
+      },
+      {} as TCurrentApePositions,
+    );
+  }),
   getClosedApePositions: publicProcedure.query(async () => {
     const { closedApePositions } = await getClosedApePositions();
 
