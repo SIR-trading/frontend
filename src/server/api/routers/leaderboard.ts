a61202daddde64ccd7774a3e8b69fd6c8acf7494
James Osunkiyesi <jaydhales@gmail.com>
2025-07-29 04:06:24 +0100

    feat: simplify API fetch calls in leaderboard by removing environment variable dependency

--- src/server/api/routers/leaderboard.ts
+++ src/server/api/routers/leaderboard.ts
@@ -3,16 +3,12 @@ import type { TPositionsResponse } from "@/lib/types";
 
 export const leaderboardRouter = createTRPCRouter({
   getClosedApePositions: publicProcedure.query(async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000"}/api/leaderboard`,
-    );
+    const res = await fetch(`/api/leaderboard`);
     const data = (await res.json()) as TPositionsResponse;
     return data.closedApePositions;
   }),
   getActiveApePositions: publicProcedure.query(async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000"}/api/leaderboard`,
-    );
+    const res = await fetch(`/api/leaderboard`);
     const data = (await res.json()) as TPositionsResponse;
     return data.activeApePositions;
   }),
