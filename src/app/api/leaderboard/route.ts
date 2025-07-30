b2901245f848d70c6ff90ecf06e6c84fda5af813
James Osunkiyesi <jaydhales@gmail.com>
2025-07-30 05:04:00 +0100

    feat: update Redis client to use Upstash and adjust cache settings

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,10 +1,14 @@
 import { getActiveApePositions } from "@/server/leaderboard/getActiveApePositions";
-import { createClient } from "redis";
+import { Redis } from "@upstash/redis";
 import { NextResponse } from "next/server";
 import { env } from "@/env";
+import { createClient } from "redis";
 
 const redis = await createClient({ url: env.REDIS_URL }).connect();
-
+// const redis = new Redis({
+//   url: env.KV_REST_API_URL,
+//   token: env.KV_REST_API_TOKEN,
+// });
 const CACHE_KEY = "leaderboard:closedPositions";
 
 export async function GET() {
@@ -20,6 +24,7 @@ export async function GET() {
   console.log("Fetched new leaderboard positions");
   await redis.set(CACHE_KEY, JSON.stringify(result), {
     EX: 60 * 30, // Cache for 10 minutes
+    // ex: 60 * 30, // Cache for 10 minutes
   });
   return NextResponse.json(result);
 }
